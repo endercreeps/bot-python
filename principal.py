d2da33fa4d77dd2168378bf7367c082c2df87150
SapGro <54324533+SapGro@users.noreply.github.com>
2019-11-23 19:03:06 +0100

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix= "!")
 
+
+extensions_initiales=["cogs.modération"]
+
+
+if __name__ == '__main__':
+    for extension in extensions_initiales:
+        bot.load_extension(extension)
+
+bot.remove_command('help')
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    print(f'Connected!')
+    
+bot.run(TOKEN)
